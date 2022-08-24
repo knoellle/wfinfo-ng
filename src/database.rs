d703acb8d5df64ca154ab53904905fcaad2143af
Konrad Nölle <konrad@familie-noelle.de>
2022-08-24 11:32:36 +0200

    Fix blueprint matching

--- src/database.rs
+++ src/database.rs
@@ -9,13 +9,13 @@ use serde_aux::field_attributes::deserialize_number_from_string;
 
 #[derive(Clone, Debug, Deserialize)]
 pub struct Item {
-    name: String,
+    pub name: String,
     #[serde(deserialize_with = "deserialize_number_from_string")]
-    yesterday_vol: u32,
+    pub yesterday_vol: u32,
     #[serde(deserialize_with = "deserialize_number_from_string")]
-    today_vol: u32,
+    pub today_vol: u32,
     #[serde(deserialize_with = "deserialize_number_from_string")]
-    custom_avg: f32,
+    pub custom_avg: f32,
 }
 
 pub struct Database {
@@ -26,19 +26,29 @@ impl Database {
     pub fn load_from_file(file: Option<&Path>) -> Database {
         // download file from: https://api.warframestat.us/wfinfo/prices
         let text = read_to_string(file.unwrap_or_else(|| Path::new("prices.json"))).unwrap();
-        let items: Vec<Item> = serde_json::from_str(&text).unwrap();
+        let mut items: Vec<Item> = serde_json::from_str(&text).unwrap();
+
+        items.iter_mut().for_each(|item| {
+            item.name = item
+                .name
+                .replace("Systems", "Systems Blueprint")
+                .replace("Neuroptics", "Neuroptics Blueprint")
+                .replace("Chassis", "Chassis Blueprint")
+        });
 
         Database { items }
     }
 
     pub fn find_item(&self, needle: &str, threshold: Option<usize>) -> Option<&Item> {
         let best_match = self.items.iter().min_by_key(|item| {
+            #[cfg(test)]
             println!(
                 "{} {} -> {}",
                 item.name,
                 needle,
                 levenshtein(&item.name, needle)
             );
+
             levenshtein(&item.name, needle)
         });
 
@@ -89,5 +99,10 @@ mod test {
             .find_item("ctavio Prlme Blueprnt", None)
             .expect("Failed to fuzzy find Octavia Prime Blueprint in database");
         assert_eq!(item.name, "Octavia Prime Blueprint");
+
+        let item = db
+            .find_item("Oclavia Prime Syslems\nBlueprint\n", None)
+            .expect("Failed to fuzzy find Octavia Prime Blueprint in database");
+        assert_eq!(item.name, "Octavia Prime Systems Blueprint");
     }
 }
