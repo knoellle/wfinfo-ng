523a46e5bd9bf5274498e931081e11f319191bd0
knoellle <konrad@familie-noelle.de>
2022-10-14 01:16:28 +0200

    Add bruteforced result to output

--- src/bin/relics.rs
+++ src/bin/relics.rs
@@ -2,7 +2,7 @@ use wfinfo::{database::Database, wfinfo_data::item_data::Refinement};
 
 fn main() {
     let database = Database::load_from_file(None, None);
-    let (name, relic) = database.relics.neo.iter().next().unwrap();
+    let (name, relic) = ("Neo N12", &database.relics.neo["N12"]);
     println!("Neo {} {:#?}", name, relic);
     println!(
         "Single: {} {}",
@@ -12,5 +12,9 @@ fn main() {
     println!(
         "Four: {}",
         database.shared_relic_value(relic, Refinement::Radiant, 4),
+    );
+    println!(
+        "Four bruteforce: {}",
+        database.shared_relic_value_bruteforce(relic, Refinement::Radiant, 4),
     );
 }
