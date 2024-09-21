a4199b73ae03e845b3aabd68cbd637e597799455
Jonas Fenchel <locken.kop@web.de>
2024-09-21 13:56:37 +0200

    simplify window name arg handling

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -159,14 +159,14 @@ struct Arguments {
     ///
     /// some systems may require the window name to be specified (e.g. when using gamescope)
     #[arg(short, long, default_value = "Warframe")]
-    window_name: Option<String>,
+    window_name: String,
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let arguments = Arguments::parse();
     let default_log_path = PathBuf::from_str(&std::env::var("HOME").unwrap()).unwrap().join(PathBuf::from_str(".local/share/Steam/steamapps/compatdata/230410/pfx/drive_c/users/steamuser/AppData/Local/Warframe/EE.log")?);
     let log_path = arguments.game_log_file_path.unwrap_or(default_log_path);
-    let window_name = arguments.window_name.unwrap();
+    let window_name = arguments.window_name;
     let windows = Window::all()?;
     let db = Database::load_from_file(None, None);
     let Some(warframe_window) = windows.iter().find(|x| x.title() == window_name) else {
