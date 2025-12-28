2d65be284f5dd798aa2bb9aaa625dfe8b3f1455a
Luke Cesario <luke.cesario@yahoo.com>
2025-12-27 23:51:32 -0600

    Require 200 status code before overwriting saved API response

--- src/utils.rs
+++ src/utils.rs
@@ -18,12 +18,15 @@ fn download_and_save(url: &str, filename: &str) -> Result<PathBuf, anyhow::Error
     }
 
     let res = reqwest::blocking::get(url)?;
-    let mut file = OpenOptions::new()
-        .write(true)
-        .truncate(true)
-        .create(true)
-        .open(&path)?;
-    file.write_all(res.text()?.as_bytes())?;
+    if res.status() == 200 {
+
+        let mut file = OpenOptions::new()
+            .write(true)
+            .truncate(true)
+            .create(true)
+            .open(&path)?;
+        file.write_all(res.text()?.as_bytes())?;
+    }
 
     Ok(path)
 }
