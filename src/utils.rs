ef0df4054ea1c6ffa09197528bb326b13faefcc8
Luke Cesario <luke.cesario@yahoo.com>
2026-05-15 23:22:41 -0500

    fix formatting

--- src/utils.rs
+++ src/utils.rs
@@ -19,7 +19,6 @@ fn download_and_save(url: &str, filename: &str) -> Result<PathBuf, anyhow::Error
 
     let res = reqwest::blocking::get(url)?;
     if res.status() == 200 {
-
         let mut file = OpenOptions::new()
             .write(true)
             .truncate(true)
